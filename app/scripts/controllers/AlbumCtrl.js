5fb4b2d7939d286f637b80f2cacd427f699a2ac5
Pierre <peberthier@gmail.com>
2017-11-17 14:47:05 +0100

    controllers attempt 2

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,7 @@
 (function() {
   function AlbumCtrl() {
-    this.songs = [];
-    for (var i=0; i < albumPicasso.length; i++) {
-      this.songs.push(angular.copy(albumPicasso.songs[i]));
+    this.albumData = albumPicasso;
     }
-    }
-  }
 
   angular
         .module('blocJams')
