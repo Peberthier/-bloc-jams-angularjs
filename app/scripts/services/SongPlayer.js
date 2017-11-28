ad9fa2c145700f76f1910d71fb1dbb447b4326fb
Pierre <peberthier@gmail.com>
2017-11-28 17:16:50 +0100

    services_part2 check point and assignement

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,16 +1,59 @@
 (function() {
     function SongPlayer() {
       var SongPlayer = {};
+      console.log('test 1')
+      var currentSong = null;
+      var currentBuzzObject = null;
+/**
+* @function setSong
+* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+* @param {Object} song
+*/
+
+var setSong = function(song) {
+  if (currentBuzzObject) {
+    currentBuzzObject.stop();
+    currentSong.playing = null;
+  };
+
+  currentBuzzObject = new buzz.sound(song.audioUrl, {
+    formats: ['mp3'],
+    preload: true
+  });
+
+  currentSong = song;
+}
+
+/**
+* @function playSong
+* @desc start playing a song and set the playing property to true
+* @param {Object} song
+*/
+
+var playSong = function(song) {
+  currentBuzzObject.play();
+  song.playing = true;
+  console.log('play test')
+}
 
       SongPlayer.play = function(song) {
-        console.log("test");
-        var currentBuzzObject = new buzz.sound(song.audioUrl, {
-          formats: ['mp3'],
-          preload: true
-        });
-
+        if (currentSong !== song) {
+          setSong(song);
+          console.log('test-play')
         currentBuzzObject.play();
+        song.playing = true;
+      } else if (currentSong === song) {
+        if (currentBuzzObject.isPaused()) {
+          playSong(song);
+        }
       }
+    };
+
+    SongPlayer.pause = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
+      console.log('test')
+    }
 
       return SongPlayer;
     }
